beaebebd5ffd54e963a63fa06e207732a3331b3c
Remco Speekenbrink <98remco@gmail.com>
2023-02-05 02:47:55 +0000

    Release 1.0.1

--- mod.cpp
+++ mod.cpp
@@ -1,6 +1,6 @@
-name = "Fish Camo Cream 1.0.0";
+name = "Fish Camo Cream 1.0.1";
 picture = "logo.paa";
-description = "Fish Camo Cream - Version 1.0.0";
+description = "Fish Camo Cream - Version 1.0.1";
 logo = "logo.paa";
 logoOver = "logo.paa";
 tooltip = "FISH_CAMO_CREAM";
